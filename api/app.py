4bdeeddd745f0ad84d58ab81a3c2d44d0cd1ae83
afcj8 <contatoantoniojunior8@gmail.com>
2025-05-21 20:35:48 -0300

    app configurado e rota get e post contatos

--- api/app.py
+++ api/app.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from api.database import create_db_and_tables
+from api.routes import router
 
 app = FastAPI(
     title="Agenda Telefônica API",
     version="0.1.0",
     description="API para gerenciar contatos de uma agenda telefônica",
 )
+
+@app.on_event("startup")
+def on_startup():
+    """Executa na inicialização do aplicativo."""
+    create_db_and_tables()
+
+@app.on_event("shutdown")
+def on_shutdown():
+    """Executa no encerramento do aplicativo."""
+    pass
+
+# Inclui as rotas no app
+app.include_router(router)
--- api/routes.py
+++ api/routes.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, HTTPException, Response
 from sqlmodel import Session, select
 
 from api.database import SessionDep
 from api.models import Contato
-from api.serializers import ContatoResponse
+from api.serializers import ContatoResponse, ContatoRequest
 
 router = APIRouter()
 
@@ -11,8 +11,31 @@ router = APIRouter()
 async def listar_contatos(
     *,
     session: Session = SessionDep,
-    response: Response
 ):
     """Lista todos os contatos"""
     contatos = session.exec(select(Contato)).all()
     return contatos
+
+@router.post("/contatos", response_model=ContatoResponse, status_code=201)
+async def criar_contato(
+    *,
+    contato: ContatoRequest,
+    session: Session = SessionDep,
+):
+    """Cria um novo contato"""
+    
+    # Verifica se o contato já existe
+    contato_existente = session.exec(
+        select(Contato).where(Contato.telefone == contato.telefone)
+    ).first()
+    if contato_existente:
+        raise HTTPException(
+            status_code=409,
+            detail="Contato com este telefone já existe.",
+        )
+        
+    contato_obj = Contato.model_validate(contato)
+    session.add(contato_obj)
+    session.commit()
+    session.refresh(contato_obj)
+    return contato_obj
