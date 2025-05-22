45b6d06c6cf1c24325bfb3e47e8ea4283e460e07
afcj8 <contatoantoniojunior8@gmail.com>
2025-05-22 09:44:00 -0300

    troca do evento obsoleto on_event pela funcao lifespan

--- api/app.py
+++ api/app.py
@@ -2,21 +2,21 @@ from fastapi import FastAPI
 from api.database import create_db_and_tables
 from api.routes import router
 
+def lifespan(app: FastAPI):
+    """Função de ciclo de vida da aplicação."""
+    
+    # Executa na inicialização da aplicação
+    create_db_and_tables()
+    yield  # Separa a inicialização do encerramento
+    # Executa no encerramento da aplicação
+    pass
+
 app = FastAPI(
     title="Agenda Telefônica API",
     version="0.1.0",
     description="API para gerenciar contatos de uma agenda telefônica",
+    lifespan=lifespan,
 )
-
-@app.on_event("startup")
-def on_startup():
-    """Executa na inicialização do aplicativo."""
-    create_db_and_tables()
-
-@app.on_event("shutdown")
-def on_shutdown():
-    """Executa no encerramento do aplicativo."""
-    pass
 
 # Inclui as rotas no app
 app.include_router(router, tags=["contatos"])
